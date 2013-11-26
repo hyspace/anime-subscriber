0d0ef59d1a8a69621ef70c10dc6e84b94a602589
shan zhou <hyspace@gmail.com>
2013-11-27 05:09:58 +0800

    add log

--- cron.js
+++ cron.js
@@ -3,6 +3,7 @@ var CronJob = require('cron').CronJob
   , subscriber = require('./anime-subscriber');
 
 var job = new CronJob('00 */5 * * * *', function(){
+    logger.log('info','Job start. '+ (new Date()).toLocaleTimeString())
     subscriber.run();
   },null,true
 );
